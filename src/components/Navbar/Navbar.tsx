c4c6c2f73a533abd78c08471f2476fcf7e15b651
cxalem <smarketing.cuz@gmail.com>
2022-05-10 21:27:13 -0500

    navbar logic

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,29 +1,67 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Disclosure } from "@headlessui/react";
 
 type Props = {};
 
 const Navbar: React.FC<Props> = ({}) => {
   return (
-    <header className=''>
-        <nav className='flex items-center'>
-            <Image 
-                src="/"
-                alt="Mag-Thebay"
-                width="100"
-                height="100"
-            />
-            <ul className='flex'>
-                <li><Link href="#"><a>Inicio</a></Link></li>
-                <li><Link href="#"><a>Nosotros</a></Link></li>
-                <li><Link href="#"><a>Proyectos</a></Link></li>
-            </ul>
-            <button>Sé voluntario</button>
-            <button>Contáctanos</button>
-        </nav>
-    </header>
-  )
-}
+    <header className="">
+      <Disclosure as="nav">
+        {({ open }) => (
+          <>
+            <div className="hidden md:flex items-center">
+              <Image src="/" alt="Mag-Thebay" width="100" height="100" />
+              <ul className="flex">
+                <li>
+                  <Link href="#">
+                    <a>Inicio</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>Nosotros</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>Proyectos</a>
+                  </Link>
+                </li>
+              </ul>
+              <button>Sé voluntario</button>
+              <button>Contáctanos</button>
+            </div>
 
-export default Navbar
+            <div className="flex justify-between md:hidden">
+              <Image src="/" alt="Mag-Thebay" width="100" height="100" />
+              <Disclosure.Button>{!open ? "Menu" : "Cerrar"}</Disclosure.Button>
+            </div>
+            <Disclosure.Panel className="flex flex-col md:hidden">
+              <div className="flex flex-col">
+                <Disclosure.Button>
+                  <Link href="#">
+                    <a>Inicio</a>
+                  </Link>
+                </Disclosure.Button>
+                <Disclosure.Button>
+                  <Link href="#">
+                    <a>Nosotros</a>
+                  </Link>
+                </Disclosure.Button>
+                <Disclosure.Button>
+                  <Link href="#">
+                    <a>Proyectos</a>
+                  </Link>
+                </Disclosure.Button>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </header>
+  );
+};
+
+export default Navbar;
