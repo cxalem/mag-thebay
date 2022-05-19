460f7af9403471986fba76396a313e0e1d18e308
cxalem <smarketing.cuz@gmail.com>
2022-05-19 00:55:57 -0500

    changes saved

--- src/components/Projects/ProjectsSection.tsx
+++ src/components/Projects/ProjectsSection.tsx
@@ -25,7 +25,7 @@ const getItems = () => {
 };
 
 const ProjectsSection: React.FC<Props> = ({}) => {
-  const projects = getItems();
+  const projects: Proyect[] = getItems();
 
   return (
     <div className="grid grid-cols-1 justify-items-center my-10 md:mb-32 md:grid-cols-2 lg:grid-cols-3 gap-10 md:px-20">
